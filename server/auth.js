bdf8295f8785c1fae72caad3f12c4501fafff8e0
djconcha12 <58573400+djconcha12@users.noreply.github.com>
2023-01-24 19:54:44 -0500

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@ const socketManager = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
 //    TODO: replace with your own CLIENT_ID
-const CLIENT_ID = "FILL IN CLIENT ID";
+const CLIENT_ID = "474140666473-hpqbbndboq5vglsoghl5j07lfqvtlkpt.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
